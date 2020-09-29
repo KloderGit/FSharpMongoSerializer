0fe18fd319d38507f93d306fedd0e11bb37a1f77
KloderGit <idzhyan.develop@outlook.com>
2020-09-29 11:52:36 +0300

    Leave just BsonSerializer Update version to .Net Standart 2.0

--- NamelessInteractive.FSharp.Utilities/Maybe.fs
+++
@@ -1,9 +0,0 @@
-﻿module NamelessInteractive.FSharp.Utilities.Maybe
-
-type MaybeBuilder() =
-    
-    member this.Bind(m,f) = Option.bind f m
-
-    member this.Return(m) = Some(m)
-
-let maybe = MaybeBuilder()
